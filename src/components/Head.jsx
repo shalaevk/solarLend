3adbab277443646582fbfe844ed981c25e29f946
shalaevk <shalaevk1987@gmail.com>
2021-07-04 16:01:07 +0300

    Change header

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -14,10 +14,10 @@ const Head = (props) => {
 
         >
             <meta charSet="utf-8" />
-            <title>{lang === "uk" ? "Сонячні електростанції в Україні | Solarland" : "Солнечные электростанции в Украине | Solarland"}</title>
+            <title property="og:title">{lang === "uk" ? "Сонячні електростанції в Україні | Solarland" : "Солнечные электростанции в Украине | Solarland"}</title>
             <link rel="icon" href={favicon} />
-            <meta name="description" content={lang === "uk" ? "Сонячні електростанції в Україні" : "Солнечные электростанции в Украине"} />
-            <meta property="og:image" content="https://solarland.s-tet.top/wp-content/uploads/2021/07/solar-logo.png" />
+            <meta property="og:description" name="description" content={lang === "uk" ? "Сонячні електростанції в Україні" : "Солнечные электростанции в Украине"} />
+            <meta property="og:image" content="../static/solar-logo.png" />
         </Helmet>
     )
 }
